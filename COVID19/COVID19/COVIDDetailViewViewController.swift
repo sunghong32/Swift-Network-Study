49dfcb37bbedea304f9ffa9c1b1ecfd9cd217f96
민성홍 <sunghong32@naver.com>
2022-04-13 21:38:08 +0900

    make UI

---
+++ COVID19/COVID19/COVIDDetailViewViewController.swift
@@ -0,0 +1,33 @@
+//
+//  COVIDDetailViewViewController.swift
+//  COVID19
+//
+//  Created by 민성홍 on 2022/04/13.
+//
+
+import UIKit
+
+class COVIDDetailViewViewController: UITableViewController {
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasinflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+
+
+    // MARK: - Navigation
+
+
+
+}
