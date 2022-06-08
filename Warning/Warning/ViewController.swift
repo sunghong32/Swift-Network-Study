bd734e950d268b7aaf97389985b0230bc428a470
민성홍 <sunghong32@naver.com>
2022-06-08 20:43:06 +0900

    [Warning] install pod, import Firebase, init firebase

---
+++ Warning/Warning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Warning
+//
+//  Created by 민성홍 on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
