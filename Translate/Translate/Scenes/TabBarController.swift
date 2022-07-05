a94c576b50258a25e79d3b7f003fea5612e39af6
민성홍 <sunghong32@naver.com>
2022-07-05 22:10:38 +0900

    [Tranlate] 입력화면 구현하기 1

---
+++ Translate/Translate/Scenes/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  Translate
+//
+//  Created by 민성홍 on 2022/07/05.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let translateViewController = TranslateViewController()
+        translateViewController.tabBarItem = UITabBarItem(
+            title: "번역",
+            image: UIImage(systemName: "mic"),
+            selectedImage: UIImage(systemName: "mic.fill")
+        )
+        
+        let bookmarkViewController = UIViewController()
+        bookmarkViewController.tabBarItem = UITabBarItem(
+            title: "즐겨찾기",
+            image: UIImage(systemName: "star"),
+            selectedImage: UIImage(systemName: "star.fill")
+        )
+        
+        viewControllers = [translateViewController, bookmarkViewController]
+    }
+}
+
